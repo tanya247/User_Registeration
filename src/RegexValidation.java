565d36bfa03e6c672ded63dfd781ee6b2325fb0a
tanya <tanyakansal238@gmail.com>
2021-04-27 12:53:17 +0530

    UC2 showed

--- src/RegexValidation.java
+++ src/RegexValidation.java
@@ -1,10 +1,10 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class RegexValidation {
-	String validFirstName = "^[A-Z]{1}[a-z]{2,}";
-	boolean validate(String FirstName) {
-		Pattern pattern = Pattern.compile(validFirstName);
-		Matcher match = pattern.matcher(FirstName);
+	String validLastName = "^[A-Z]{1}[a-z]{2,}";
+	boolean validate(String LastName) {
+		Pattern pattern = Pattern.compile(validLastName);
+		Matcher match = pattern.matcher(LastName);
 		boolean b = match.matches();
 		return b;
 	}
--- src/UserRegisterationValidation.java
+++ src/UserRegisterationValidation.java
@@ -6,22 +6,22 @@ public class UserRegisterationValidation {
 		
 		
 		
-		firstName();
+		lastName();
 	}
-	static String firstName() {
+	static String lastName() {
 		RegexValidation jr = new RegexValidation();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name");
-		String FirstName = sc.nextLine();
-		boolean r = jr.validate(FirstName);
+		System.out.println("Enter Last Name");
+		String LastName = sc.nextLine();
+		boolean r = jr.validate(LastName);
 		if(r) {
 			System.out.println("Name is valid");
-			return FirstName;
+			return LastName;
 			
 		}
 		else {
 			System.out.println("Name is not valid , Enter correct Name");
-			return firstName();
+			return lastName();
 		}
 	}
 
