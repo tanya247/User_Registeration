3498da8ca4ae0f2837363fbcc821c1235bfa01e7
tanya <tanyakansal238@gmail.com>
2021-04-27 14:27:27 +0530

    UC3 showed

--- src/RegexValidation.java
+++ src/RegexValidation.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 public class RegexValidation {
 	String validFirstName = "^[A-Z]{1}[a-z]{2,}";
 	String validLastName = "^[A-Z]{1}[a-z]{2,}";
+	String validEmailAddress ="^[a-zA-Z0-9]{1,}[.]{0,1}[a-zA-Z0-9]{0,}+@[a-zA-Z0-9]+.[a-zA-Z]{2,3}[.]{0,1}[a-zA-Z]{0,}";
+	
 	boolean validateFirstName(String FirstName) {
 		Pattern pattern = Pattern.compile(validFirstName);
 		Matcher match = pattern.matcher(FirstName);
@@ -15,4 +17,11 @@ public class RegexValidation {
 		boolean b = match.matches();
 		return b;
 	}
+	boolean validateEmailAddress(String EmailAddress) {
+		Pattern pattern = Pattern.compile(validEmailAddress);
+		Matcher match = pattern.matcher(EmailAddress);
+		boolean b = match.matches();
+		return b;
+	}
+	
 }
--- src/UserRegisterationValidation.java
+++ src/UserRegisterationValidation.java
@@ -5,6 +5,7 @@ public class UserRegisterationValidation {
 	public static void main(String args[]) {
 		firstName();
 		lastName();
+		emailAddress();
 	}
 	static String firstName() {
 		RegexValidation jr = new RegexValidation();
@@ -38,5 +39,21 @@ public class UserRegisterationValidation {
 			return lastName();
 		}
 	}
-
+	static String emailAddress() {
+		RegexValidation jr = new RegexValidation();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Email Address");
+		String EmailAddress = sc.nextLine();
+		boolean r = jr.validateEmailAddress(EmailAddress);
+		if(r) {
+			System.out.println("Email Address is valid");
+			return EmailAddress;
+			
+		}
+		else {
+			System.out.println("Email Address is not valid , Enter correct Email Adress");
+			return emailAddress();
+		}
+	}
+	
 }
