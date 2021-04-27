0695357d4e121a8c4687d00182eb3aee978003bb
tanya <tanyakansal238@gmail.com>
2021-04-27 13:37:45 +0530

    UC3 showed

--- src/RegexValidation.java
+++ src/RegexValidation.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 public class RegexValidation {
 	String validFirstName = "^[A-Z]{1}[a-z]{2,}";
 	String validLastName = "^[A-Z]{1}[a-z]{2,}";
+	String validEmailAddress ="^[a-z0-9][a-z\\.]+@[a-z0-9]+[\\.]+[a-z\\.]";
 	boolean validateFirstName(String FirstName) {
 		Pattern pattern = Pattern.compile(FirstName);
 		Matcher match = pattern.matcher(FirstName);
@@ -12,6 +13,12 @@ public class RegexValidation {
 	boolean validateLastName(String LastName) {
 		Pattern pattern = Pattern.compile(validLastName);
 		Matcher match = pattern.matcher(LastName);
+		boolean b = match.matches();
+		return b;
+	}
+	boolean validateEmailAddress(String EmailAddress) {
+		Pattern pattern = Pattern.compile(validEmailAddress);
+		Matcher match = pattern.matcher(EmailAddress);
 		boolean b = match.matches();
 		return b;
 	}
